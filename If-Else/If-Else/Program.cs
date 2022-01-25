a2defb80a37c5807f0050d8c752d393c88a03d0f
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-01-25 15:34:14 +0300

    Add files via upload

---
+++ If-Else/If-Else/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+             if (time < 12)
+            {
+                Console.WriteLine("Günaydınn ... :) :) :)");
+            }
+             else if (time > 12)
+            {
+                Console.WriteLine("İyi Günler ... :) :) :) ");
+            }
+            else if ( time > 18)
+            {
+                Console.WriteLine(" İyi Akşamlar ... :) :) :)");
+            }
+
+            string sonuc = time <= 18 ? "İyi Günler.." : "İyi Geceler..";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
